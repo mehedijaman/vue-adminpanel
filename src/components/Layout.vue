6f753ae6e0d495f747454728110ee10ecac592e3
Mehedi Jaman <mail4mjaman@gmail.com>
2023-10-03 21:00:18 +0600

    - Product CRUD Page added

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -6,6 +6,10 @@ import Footer from './Footer.vue';
 <template>
     <Header></Header>
     <Sidebar></Sidebar>
-    <slot></slot>
+    <div class="p-4 sm:ml-64">
+        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+            <slot></slot>
+        </div>
+    </div>
     <Footer></Footer>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 const Home = () => import('../views/Home.vue');
+
 const Login = () => import('../views/Login.vue');
 const Register = () => import('../views/Register.vue');
+const PasswordForget = () => import('../views/PasswordForget.vue');
+const PasswordReset = () => import('../views/PasswordReset.vue');
+
 const Dashboard = () => import('../views/Dashboard.vue');
 const Products = () => import('../views/Products.vue');
+const ProductsCreate = () => import('../components/Product/CreateProduct.vue');
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,6 +29,11 @@ const router = createRouter({
       component: Products
     },
     {
+      path: '/product-create',
+      name: 'product-create',
+      component: ProductsCreate
+    },
+    {
       path: '/login',
       name: 'login',
       component: Login
@@ -32,6 +42,16 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: Register
+    },
+    {
+      path: '/password-forget',
+      name: 'password-forget',
+      component: PasswordForget
+    },
+    {
+      path: '/password-reset',
+      name: 'password-reset',
+      component: PasswordReset
     },
   ]
 })
